93d30950e57ac67296b471a8af7cb44d3717d9f8
Nathalie <nathalie.cochard7@gmail.com>
2016-06-01 18:56:42 +0000

    finish login, logout, register, password, email validation etc.

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "noreply@uni2choose.com"
+  layout 'mailer'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "Nathalie Cochard",
+             email: "nathalie.cochard7@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+             
+User.create!(name:  "Bruce Scharlau",
+             email: "b.scharlau@abdn.ac.uk",
+             password:              "RoR-master",
+             password_confirmation: "RoR-master",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+             
+User.create!(name:  "Example User",
+             email: "example@user.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin:     false,
+             activated: true,
+             activated_at: Time.zone.now)
+
+40.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@uni2choose.com"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               admin: false,              
+               activated: true,
+               activated_at: Time.zone.now)
+
+end

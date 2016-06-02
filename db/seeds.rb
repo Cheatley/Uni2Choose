8f5b83b74980041f12037d5b02b9af818b1b4438
Nathalie <nathalie.cochard7@gmail.com>
2016-06-02 14:05:40 +0000

    finish change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,39 +10,48 @@ User.create!(name:  "Nathalie Cochard",
             age: 22,
             gender: "female",
             postcode: "AB24 3EX",
-             email: "nathalie.cochard7@gmail.com",
-             password:              "password",
-             password_confirmation: "password",
-             admin:     true,
-             activated: true,
-             activated_at: Time.zone.now)
+            email: "nathalie.cochard7@gmail.com",
+            password:              "password",
+            password_confirmation: "password",
+            admin:     true,
+            activated: true,
+            activated_at: Time.zone.now)
              
 User.create!(name:  "Bruce Scharlau",
-             email: "b.scharlau@abdn.ac.uk",
-             password:              "RoR-master",
-             password_confirmation: "RoR-master",
-             admin:     true,
-             activated: true,
-             activated_at: Time.zone.now)
+            age: 24,
+            gender: "male",
+            postcode: "AB10 9FG",
+            email: "b.scharlau@abdn.ac.uk",
+            password:              "RoR-master",
+            password_confirmation: "RoR-master",
+            admin:     true,
+            activated: true,
+            activated_at: Time.zone.now)
              
 User.create!(name:  "Example User",
-             email: "example@user.com",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin:     false,
-             activated: true,
-             activated_at: Time.zone.now)
+            age: 15,
+            gender: "female",
+            postcode: "AB24 3EX",
+            email: "example@user.com",
+            password:              "foobar",
+            password_confirmation: "foobar",
+            admin:     false,
+            activated: true,
+            activated_at: Time.zone.now)
 
 40.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@uni2choose.com"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               admin: false,              
-               activated: true,
-               activated_at: Time.zone.now)
+              age: 14,
+              gender: "female",
+              postcode: "AB24 3EX",
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              admin: false,              
+              activated: true,
+              activated_at: Time.zone.now)
 
 end

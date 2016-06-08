1d1602bcead5376d59f9b3a4216645339ae6ae84
Nathalie <nathalie.cochard7@gmail.com>
2016-06-08 10:22:13 +0000

    like/ilike

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,9 @@
 class Course < ActiveRecord::Base
   def self.search(search)
-    where("course_title LIKE :p", p: "%#{search}%")
+    where("course_title ILIKE :p", p: "%#{search}%")
   end
   
   def self.gradesearch(grades)
-    where("highers LIKE :p", p: "%#{grades}%")
+    where("highers ILIKE :p", p: "%#{grades}%")
   end
 end

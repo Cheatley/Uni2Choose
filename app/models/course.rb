ff66160d89edb4910d80d129f6cd6f96ac349bcc
Nathalie <nathalie.cochard7@gmail.com>
2016-06-09 14:01:11 +0000

    added radio buttons no functionality behind

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,10 @@
 class Course < ActiveRecord::Base
   def self.search(search)
-    where("course_title LIKE :p", p: "%#{search}%")
+    where("course_title ILIKE :p", p: "%#{search}%")
   end
   
   def self.advanced_search(interests)
-    where('departments LIKE ? OR course_title LIKE ?', "%#{interests}%", "%#{interests}%")
+    where('departments ILIKE ? OR course_title ILIKE ?', "%#{interests}%", "%#{interests}%")
   end
 
 end

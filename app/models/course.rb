fafe333367e2997c33d6761fa88782ca1de1f26f
Nathalie <nathalie.cochard7@gmail.com>
2016-06-10 14:42:15 +0000

    Katie's and Lee's push

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,10 @@
 class Course < ActiveRecord::Base
   def self.search(search)
-    where("course_title ILIKE :p", p: "%#{search}%")
+    where("course_title LIKE :p", p: "%#{search}%")
   end
   
   def self.advanced_search(interests)
-    where('departments ILIKE ? OR course_title ILIKE ?', "%#{interests}%", "%#{interests}%")
+    where('departments LIKE ? OR course_title LIKE ?', "%#{interests}%", "%#{interests}%")
   end
 
 end

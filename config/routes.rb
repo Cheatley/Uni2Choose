989f55d32294a528ef44f967a3d3ab9455b8ac89
Nathalie <nathalie.cochard7@gmail.com>
2016-06-16 13:28:59 +0000

    finish redirect to summer school if no  standard grades checked

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
-
-
  root 'pages#home'
- 
+ get 'summerschool' => 'pages#summerschool'
  get 'signup'  => 'users#new'
  get 'login'   => 'sessions#new'
  post 'login'   => 'sessions#create'
@@ -12,10 +10,9 @@ Rails.application.routes.draw do
  get 'courses' => 'courses#index'
  get 'keyword_search' => 'courses#keyword_search'
  get 'advanced_search' => 'courses#advanced_search'
-
- #resources :searches
- resources :searches
  
+ resources :searches
+ get 'recommender_search' => 'searches#new'
  resources :users
  resources :account_activations, only: [:edit]
  resources :password_resets, only: [:new, :create, :edit, :update]

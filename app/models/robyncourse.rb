3803bb615c3b0f191ef10b3899ee154f2c680787
Nathalie <nathalie.cochard7@gmail.com>
2016-06-24 11:28:40 +0000

    changes due to add/remove dbs

--- app/models/robyncourse.rb
+++ app/models/robyncourse.rb
@@ -1,7 +1,5 @@
 class Course < ActiveRecord::Base
-  def self.search(search)
-    where("course_title LIKE :p", p: "%#{search}%")
-  end
+
   
   def self.advanced_search(interests)
     where('departments LIKE ? OR course_title LIKE ?', "%#{interests}%", "%#{interests}%")

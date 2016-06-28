3437977f3d82143755a71c1d62b34184f10368cb
Nathalie <nathalie.cochard7@gmail.com>
2016-06-28 15:35:52 +0000

    finish delete old authentication system

--- db/migrate/20160604121428_add_registerfields_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddRegisterfieldsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :dob, :datetime
-    add_column :users, :gender, :string
-    add_column :users, :postcode, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -90,23 +90,4 @@ ActiveRecord::Schema.define(version: 20160627110559) do
     t.string "ulogo"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",             default: false
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
-    t.datetime "activated_at"
-    t.string   "reset_digest"
-    t.datetime "reset_sent_at"
-    t.datetime "dob"
-    t.string   "gender"
-    t.string   "postcode"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-
 end

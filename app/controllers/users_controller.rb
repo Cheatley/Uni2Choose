b9f2f550ff81aede57202dc35cb5a3ac247d1f7d
Nathalie <nathalie.cochard7@gmail.com>
2016-06-30 08:27:45 +0000

    finished added admin boolean column to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      accessible = [ :name, :email ] # extend with your own params
+      accessible = [ :name, :email ] # to extend with params :dob, :postcode, :gender
       accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
       params.require(:user).permit(accessible)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,65 +6,65 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(dob: DateTime.new(1993, 9, 13),
-            gender: "Female",
-            postcode: "AB24 3EX",
+User.create!(#dob: DateTime.new(1993, 9, 13),
+            #gender: "Female",
+            #postcode: "AB24 3EX",
             email: "nathalie.cochard7@gmail.com",
             password:              "password",
             password_confirmation: "password",
             admin:     true,
-            activated: true,
-            activated_at: Time.zone.now)
+            #activated: true,
+            confirmed_at: Time.zone.now)
              
-User.create!(dob: DateTime.new(2015, 6, 22),
-            gender: "Male",
-            postcode: "AB10 9FG",
+User.create!(#dob: DateTime.new(2015, 6, 22),
+            #gender: "Male",
+            #postcode: "AB10 9FG",
             email: "b.scharlau@abdn.ac.uk",
             password:              "RoR-master",
             password_confirmation: "RoR-master",
             admin:     true,
-            activated: true,
-            activated_at: Time.zone.now)
+            #activated: true,
+            confirmed_at: Time.zone.now)
              
-User.create!(dob: DateTime.new(2015, 6, 22),
-            gender: "Female",
-            postcode: "AB24 3EX",
+User.create!(#dob: DateTime.new(2015, 6, 22),
+            #gender: "Female",
+            #postcode: "AB24 3EX",
             email: "example@user.com",
             password:              "foobar",
             password_confirmation: "foobar",
             admin:     false,
-            activated: true,
-            activated_at: Time.zone.now)
+            #activated: true,
+            confirmed_at: Time.zone.now)
 
 40.times do |n|
   email = Faker::Internet.email
   password = "password"
-  dob = Faker::Date.between(18.year.ago, 13.year.ago)
-  postcode = Faker::Address.zip_code
-  User.create!(dob: dob,
-              gender: "Female",
-              postcode: postcode,
+  #dob = Faker::Date.between(18.year.ago, 13.year.ago)
+  #postcode = Faker::Address.zip_code
+  User.create!(#dob: dob,
+              #gender: "Female",
+              #postcode: postcode,
               email: email,
               password: password,
               password_confirmation: password,
               admin: false,              
-              activated: true,
-              activated_at: Time.zone.now)
+              #activated: true,
+              confirmed_at: Time.zone.now)  
 end
 
 40.times do |n|
   email = Faker::Internet.email
   password = "password"
-  dob = Faker::Date.between(18.year.ago, 13.year.ago)
-  postcode = Faker::Address.zip_code
-  User.create!(dob: dob,
-              gender: "Male",
-              postcode: postcode,
+  #dob = Faker::Date.between(18.year.ago, 13.year.ago)
+  #postcode = Faker::Address.zip_code
+  User.create!(#dob: dob,
+              #gender: "Male",
+              #postcode: postcode,
               email: email,
               password: password,
               password_confirmation: password,
               admin: false,              
-              activated: true,
-              activated_at: Time.zone.now)
+              #activated: true,
+              confirmed_at: Time.zone.now)
 
 end

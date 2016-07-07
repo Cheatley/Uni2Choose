c05dfffd9c66c8c34527e02bf0dfb01a9b5bee9b
NinjadogAkamaru <watsonkati@hotmail.co.uk>
2016-07-07 13:11:47 +0000

    fix

---
+++ app/config/routes.rb
@@ -0,0 +1,36 @@
+Rails.application.routes.draw do
+
+ devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+ resources :users, :only => [:show, :index]
+ get 'registration/new'
+
+
+ mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+ get 'users_list' => 'rails_admin/main#show'
+
+ get 'recommends/new'
+
+ get 'degree/new'
+
+ root 'pages#home'
+ get 'about' => 'pages#about'
+ get 'new' => 'searches#new'
+ get 'quiz' => 'searches#quiz'
+ get 'summerschool' => 'pages#summerschool'
+ get 'analysis' => 'analysis#user_analysis'
+ get 'courses' => 'degree#index'
+ get 'keyword_search' => 'searches#keyword_search'
+ get 'advanced_search' => 'searches#advanced_search'
+ get 'adsearch' => 'searches#adsearch'
+ get 'ques' => 'searches#ques'
+ get 'result' => 'searches#result'
+
+ resources :searches
+ get 'recommender_search' => 'searches#new'
+
+ #contact form routes
+ match '/contacts',     to: 'contacts#new',             via: 'get'
+ resources "contacts", only: [:new, :create]
+
+
+end

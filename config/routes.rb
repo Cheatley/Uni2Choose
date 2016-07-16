cf7cba62063305bef4e751a349158b00cc776287
Nathalie <nathalie.cochard7@gmail.com>
2016-07-16 13:41:21 +0000

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+ match '/users/sign_out', to: 'devise/sessions#destroy', via: 'delete' 
  
  resources :users, :only => [:show, :index]
  
